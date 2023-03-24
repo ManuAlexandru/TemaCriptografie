9e7aa36b51327cee9d3d94208f9ca9a8e13ec8c1
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-03-24 20:48:17 +0200

    Tema3

---
+++ Tema3/PermutationEncypher.cs
@@ -0,0 +1,55 @@
+﻿namespace Tema3
+{
+    public class PermutationEncypher
+    {
+        public string Encypher(string text, List<int> permutation)
+        {
+            string enchypheredText = string.Empty;
+            int perCount = permutation.Count;
+            int newChar = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    enchypheredText += 'Q';
+                    continue;
+                }
+
+                if (!((text[i] > 64 && text[i] < 91) || (text[i] > 96 && text[i] < 123)))
+                {
+                    enchypheredText += text[i];
+                    continue;
+                }
+
+                if ((text[i] + permutation[i % perCount]) > 122)
+                {
+                    enchypheredText += EncryptLetter(text[i], permutation, i, 26, perCount);
+
+                    continue;
+                }
+
+                if ((text[i] + permutation[i % perCount]) > 90)
+                {
+                    enchypheredText += EncryptLetter(text[i], permutation, i, 26, perCount);
+
+                    continue;
+                }
+
+                enchypheredText += EncryptLetter(text[i], permutation, i, 0, perCount);
+
+            }
+
+            return enchypheredText;
+        }
+
+        private char EncryptLetter(char character, List<int> permutation, int poz, int divider, int permutationCounter)
+        {
+
+            return Convert.ToChar(character + permutation[poz % permutationCounter] - divider);
+        }
+
+
+
+    }
+}
---
+++ Tema3/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Tema3;
+
+PermutationEncypher encypher = new PermutationEncypher();
+
+string text = "ANA ARE MERE, CIRESEL VINE SI CERE.";
+string text1 = "FQBQEWHQOIWH,QGNUFUIQQWKRJQTKQHHSG.";
+var permutation = new List<int>();
+permutation.Add(5);
+permutation.Add(3);
+permutation.Add(1);
+permutation.Add(2);
+permutation.Add(4);
+
+Console.WriteLine("Textul normal: " + text);
+Console.WriteLine("Textul criptat: " + encypher.Encypher(text, permutation));
+Console.WriteLine(text1.Length);
