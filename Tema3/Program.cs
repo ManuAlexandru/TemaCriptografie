98b49af6b7dbceb9c93753b50aa4c4902eec7d67
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-03-24 20:48:37 +0200

    refactored the code

--- Tema3/Program.cs
+++ Tema3/Program.cs
@@ -4,7 +4,7 @@ using Tema3;
 PermutationEncypher encypher = new PermutationEncypher();
 
 string text = "ANA ARE MERE, CIRESEL VINE SI CERE.";
-string text1 = "FQBQEWHQOIWH,QGNUFUIQQWKRJQTKQHHSG.";
+
 var permutation = new List<int>();
 permutation.Add(5);
 permutation.Add(3);
@@ -14,4 +14,3 @@ permutation.Add(4);
 
 Console.WriteLine("Textul normal: " + text);
 Console.WriteLine("Textul criptat: " + encypher.Encypher(text, permutation));
-Console.WriteLine(text1.Length);
