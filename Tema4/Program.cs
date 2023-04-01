565298ef063f2237d81549c4f673c1f4a0691ff0
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-04-01 19:05:36 +0300

    Changed key

--- Tema4/Program.cs
+++ Tema4/Program.cs
@@ -6,7 +6,7 @@ ExerciseCypher2 cypher2 = new ExerciseCypher2();
 string text = "ManuAlexandru";
 
 var result1 = cypher1.Enchypher(13, text);
-var result2 = cypher2.Enchypher(13, text);
+var result2 = cypher2.Enchypher(9, text);
 Console.WriteLine("Sirul care va fi criptat este: " + text);
 Console.WriteLine("Sirul criptat cu prima formula este: " + result1);
 Console.WriteLine("Sirul criptat cu a doua formula este: " + result2);
