023fb88a495c11236004cb291e3f1debbb14b059
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-05-10 09:10:41 +0300

    Tema6

---
+++ Tema6/CriptareAfina.cs
@@ -0,0 +1,26 @@
+﻿namespace Tema6
+{
+    public class CriptareAfina
+    {
+        public string Enchypher(string text)
+        {
+
+            string textToEncypher = string.Empty;
+
+            string encryptedText = string.Empty;
+
+            foreach (var elem in text.Split(' '))
+            {
+                textToEncypher = textToEncypher + elem;
+            }
+
+            foreach (var elem in textToEncypher)
+            {
+                var encryptedChar = (3 * elem + 10) % 26;
+                encryptedText = encryptedText + Convert.ToChar(65 + encryptedChar);
+            }
+
+            return encryptedText;
+        }
+    }
+}
---
+++ Tema6/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tema6;
+
+CriptareAfina criptareAfina = new CriptareAfina();
+
+var result = criptareAfina.Enchypher("CRIPTOGRAFIESISECURITATE");
+
+Console.WriteLine("Sirul criptat este: " + result);
