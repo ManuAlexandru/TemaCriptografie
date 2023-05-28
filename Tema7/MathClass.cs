dfbc3f1a7853bac6175dd0ed8a2b2a18f41d02da
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-05-28 10:53:57 +0300

    Tema7

---
+++ Tema7/MathClass.cs
@@ -0,0 +1,28 @@
+﻿namespace Tema7
+{
+    public class MathClass
+    {
+        // prime modulo value
+        static long N = 1000000007L;
+
+        public static (long, long) exponentiation(long bas, long exp, long step)
+        {
+            if (exp == 0)
+                return (1, step);
+
+            if (exp == 1)
+                return (bas % N, step);
+
+            (long t, long steps) = exponentiation(bas, exp / 2, step + 1);
+            t = (t * t) % N;
+
+            // if exponent is even value
+            if (exp % 2 == 0)
+                return (t, steps + 1);
+
+            // if exponent is odd value
+            else
+                return (((bas % N) * t) % N, steps + 1);
+        }
+    }
+}
---
+++ Tema7/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Tema7;
+
+(long number, long steps) = MathClass.exponentiation(7, 13, 1);
+Console.WriteLine("Result: " + number + " " + "Number of steps: " + steps);
+
+(number, steps) = MathClass.exponentiation(17, 37, 1);
+Console.WriteLine("Result: " + number + " " + "Number of steps: " + steps);
+
+(number, steps) = MathClass.exponentiation(34, 125, 1);
+Console.WriteLine("Result: " + number + " " + "Number of steps: " + steps);
+
+(number, steps) = MathClass.exponentiation(2321, 23231, 1);
+Console.WriteLine("Result: " + number + " " + "Number of steps: " + steps);
