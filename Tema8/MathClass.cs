f7d5c1346aff78a15194885d018ac65e842aa3cc
ManuAlexandru <92298889+ManuAlexandru@users.noreply.github.com>
2023-05-28 11:46:58 +0300

    Tema8

---
+++ Tema8/MathClass.cs
@@ -0,0 +1,19 @@
+﻿namespace Tema8
+{
+    public class MathClass
+    {
+
+
+        public static int DiscretLogarithm(int b, int g, int p)
+        {
+            int x = 0;
+            var valueToCompare = (Math.Pow(g, x)) % p;
+            while (valueToCompare != b)
+            {
+                x++;
+                valueToCompare = (Math.Pow(g, x)) % p;
+            }
+            return x;
+        }
+    }
+}
---
+++ Tema8/Program.cs
@@ -0,0 +1,4 @@
+﻿using Tema8;
+
+var result = MathClass.DiscretLogarithm(8, 2, 13);
+Console.WriteLine("The Number is " + result);
